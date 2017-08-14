288c95a692361ac00dcf33ee3073c7b4fa944243
Jason Gross <jason.gross@thomsonretuers.com>
2017-08-14 19:12:49 +0100

    attempting to use local bind mounted cifs volumes instead of cloudstor plugin

--- setup.sh
+++ setup.sh
@@ -3,29 +3,55 @@
 export DOCKER_FOR_IAAS_VERSION="17.06.0-ce-azure1"
 export DOCKER_FOR_UBUNTU_VERSION="17.06.0~ce-0~ubuntu"
 
+# ensure system is up to date, add prerequisite dependencies
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common vim
+
+#set up docker repo
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce=$DOCKER_FOR_UBUNTU_VERSION
 
+#set up azure cli repo
+sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main"
+
+#refresh package list and install 
+sudo apt-get update
+sudo apt-get install -y docker-ce=$DOCKER_FOR_UBUNTU_VERSION azure-cli
+
+#update docker config
 sudo tee /etc/docker/daemon.json > /dev/null <<EOF
 {
-  "log-driver": "syslog",
-  "log-opts": {
-    "syslog-address": "udp://localhost:514",
-    "tag": "{{.Name}}/{{.ID}}"
-  },
   "storage-driver": "overlay2"
 }
 EOF
 
+#enable ssh user to run docker commands without sudo
 sudo usermod -aG docker $ADMIN_USER
+#start docker on boot
 sudo systemctl enable docker
+#bounce docker for config changes
 sudo systemctl restart docker
 
+#login to azure with service principal
+az login --service-principal -u $APP_ID -p $APP_SECRET --tenant $TENANT_ID
+#create share
+az storage share create --name $SWARM_VOLUME_SHARE --account-name $SWARM_STORAGE_ACCOUNT
+#get storage account access key 
+SA_KEY=`az storage account keys list --resource-group $GROUP_NAME --account-name $SWARM_STORAGE_ACCOUNT | python -c "import sys, json; print json.load(sys.stdin)[0]['value']"`
+#get full endpoint for storage account (not sure if this domain differs for government or not)
+SA_ENDPOINT=`az storage account show --resource-group $GROUP_NAME --name $SWARM_STORAGE_ACCOUNT | python -c "import sys, json; print json.load(sys.stdin)['primaryEndpoints']['file'].replace('/', '').split(':')[1]"`
+
+#create local mountpoint for volumes (/mnt is already used by azure)
+sudo mkdir -p /volumes
+sudo chmod 777 /volumes
+
+#update fstab for storage account
+echo "//$SA_ENDPOINT/$SWARM_VOLUME_SHARE /volumes cifs vers=3.0,username=$SWARM_STORAGE_ACCOUNT,password=$SA_KEY,dir_mode=0777,file_mode=0777,sec=ntlmssp 0 0" | sudo tee -a /etc/fstab
+sudo mount /volumes
+
+#kick off a modified version of the swarm init container
 sudo -E docker run --label com.docker.editions.system \
            --log-driver=json-file \
            --restart=no \
