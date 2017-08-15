49907609a3af4540961e36b442a4a1b1b738799d
Jason Gross <jason.gross@thomsonretuers.com>
2017-08-15 17:05:51 +0100

    pull share names from template

--- setup.sh
+++ setup.sh
@@ -36,20 +36,22 @@ sudo systemctl restart docker
 
 #login to azure with service principal
 az login --service-principal -u $APP_ID -p $APP_SECRET --tenant $TENANT_ID
-#create share
-az storage share create --name $SWARM_VOLUME_SHARE --account-name $SWARM_STORAGE_ACCOUNT
 #get storage account access key 
 SA_KEY=`az storage account keys list --resource-group $GROUP_NAME --account-name $SWARM_STORAGE_ACCOUNT | python -c "import sys, json; print json.load(sys.stdin)[0]['value']"`
 #get full endpoint for storage account (not sure if this domain differs for government or not)
 SA_ENDPOINT=`az storage account show --resource-group $GROUP_NAME --name $SWARM_STORAGE_ACCOUNT | python -c "import sys, json; print json.load(sys.stdin)['primaryEndpoints']['file'].replace('/', '').split(':')[1]"`
 
-#create local mountpoint for volumes (/mnt is already used by azure)
-sudo mkdir -p /volumes
-sudo chmod 777 /volumes
-
-#update fstab for storage account
-echo "//$SA_ENDPOINT/$SWARM_VOLUME_SHARE /volumes cifs vers=3.0,username=$SWARM_STORAGE_ACCOUNT,password=$SA_KEY,dir_mode=0777,file_mode=0777,sec=ntlmssp 0 0" | sudo tee -a /etc/fstab
-sudo mount /volumes
+for SHARE in ${SWARM_VOLUME_SHARES[@]}; do
+  #create share
+  az storage share create --name $SHARE --account-name $SWARM_STORAGE_ACCOUNT
+  #create local mountpoint for volumes (/mnt is already used by azure so we'll use /volumes)
+  sudo mkdir -p /volumes/$SHARE
+  sudo chmod 777 /volumes/$SHARE
+  
+  #update fstab for storage account
+  echo "//$SA_ENDPOINT/$SHARE /volumes/$SHARE cifs vers=3.0,username=$SWARM_STORAGE_ACCOUNT,password=$SA_KEY,dir_mode=0777,file_mode=0777,sec=ntlmssp,nobrl 0 0" | sudo tee -a /etc/fstab
+  sudo mount /volumes/$SHARE
+done
 
 #kick off a modified version of the swarm init container
 sudo -E docker run --label com.docker.editions.system \
